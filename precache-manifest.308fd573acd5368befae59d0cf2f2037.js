9e72c80a1209c7efc1e428038d760c55e4317772
James Royston <jamesroyston03@gmail.com>
2019-06-29 00:49:45 -0500

    Updates

---
+++ precache-manifest.308fd573acd5368befae59d0cf2f2037.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d55cc641ff01805dbe70dd1d0b899c2",
+    "url": "/todomvc-clone/index.html"
+  },
+  {
+    "revision": "0aa1d67023fa74a44350",
+    "url": "/todomvc-clone/static/css/main.9c2ab9f0.chunk.css"
+  },
+  {
+    "revision": "db3927a79f0d16c235fc",
+    "url": "/todomvc-clone/static/js/2.13ef48b5.chunk.js"
+  },
+  {
+    "revision": "0aa1d67023fa74a44350",
+    "url": "/todomvc-clone/static/js/main.34445751.chunk.js"
+  },
+  {
+    "revision": "61434f987bc2f428facd",
+    "url": "/todomvc-clone/static/js/runtime~main.072f2893.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todomvc-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
